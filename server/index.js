559b63661f9d47231ad00055a4970108fbacce2c
Prashik Mane <prashikmane1412@gmail.com>
2026-06-29 18:45:43 +0530

    Use Render dynamic port

--- server/index.js
+++ server/index.js
@@ -18,6 +18,8 @@ app.use("/jobs", jobRoutes);
 
 const PORT = 5000;
 
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
-    console.log(`🚀 Server running on http://localhost:${PORT}`);
+    console.log(`🚀 Server running on port ${PORT}`);
 });
