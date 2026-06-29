2d6a8055066f0a30f92c26b9b482e481f3f6c1ca
Prashik Mane <prashikmane1412@gmail.com>
2026-06-29 22:54:09 +0530

    Fix duplicate PORT declaration

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-
+console.log("DATABASE_URL =", process.env.DATABASE_URL);
 const express = require("express");
 const cors = require("cors");
 const authRoutes = require("./routes/authRoutes");
@@ -9,14 +9,14 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
 app.use("/auth", authRoutes);
+
 app.get("/", (req, res) => {
     res.send("🚀 JobVault API Running");
 });
 
 app.use("/jobs", jobRoutes);
-
-const PORT = 5000;
 
 const PORT = process.env.PORT || 5000;
 
